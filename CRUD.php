106d3384df102a895144e08ba5524109d521d874
Braun Okoi Boniface <ldbrawn@gmail.com>
2020-06-03 08:03:02 -0500

    First and Legacy Version

--- CRUD.php
+++ CRUD.php
@@ -23,6 +23,9 @@ class CRUD
         if($location != NULL) {
             header("Location: {$location}");
             exit;
+        } else {
+            header("Location: index.php");
+            exit;
         }
     }
     //End Redirection function
@@ -51,13 +54,13 @@ class CRUD
     *
     *
     */
-    function get($query) {
+    function get_rec($query) {
         global $conn;
         $sql = $query;
         $result = $conn->query($sql);
         
         if(!$result) {
-            die($conn->error);
+            throw new Exception($conn->error);
         }
         
         return $result;
@@ -83,6 +86,48 @@ class CRUD
     
     /**
     *
+    * Insert's new record to database table from form or any key=>value pair record
+    *
+    *@param string $table The database table to insert the record to
+    *@param array $post_fields http method(post, get) or key=>value pair
+    *@param array $formats Additional form field format e.g: setting encrypting password field like array("password"=>md5('password'))
+    *@param boolean $avail_btn Checks if submit button in the form has a name and is included in the post parameters
+    */
+    function add_rec($table, array $post_fields, $formats=array(), $avail_btn=true) {
+        global $conn;
+        $error = "";
+        
+        if(!empty($formats) && count($formats) > 0) {
+            foreach($formats as $fkey=>$fval) {
+                $post_fields[$fkey] = $fval;
+            }
+        }
+        
+        $fields = array();
+        $vals = array();
+
+        foreach($post_fields as $field=>$val) {
+            array_push($fields, "`{$field}`");
+            array_push($vals, "'{$conn->real_escape_string($val)}'");
+        }
+        
+        if($avail_btn === true) {
+            array_pop($fields);
+            array_pop($vals);
+        }
+
+        //Conversions
+        $cols = implode(", ", $fields);
+        $data = implode(", ", $vals);
+
+        $sql = "INSERT INTO {$table}({$cols}) VALUES ({$data})";
+        $result = $conn->query($sql) or die($conn->error);
+        
+        return $result;
+    }
+    
+    /**
+    *
     * Insert's new record to database table from form or any key=>value pair record using SQL prepared statement
     *
     *@param string $table The database table to insert the record to
@@ -90,7 +135,7 @@ class CRUD
     *@param array $formats Additional form field format e.g: setting encrypting password field like array("password"=>md5('password'))
     *@param boolean $avail_btn Checks if submit button in the form has a name and is included in the post parameters
     */
-    function add($conn, $table, array $columns, $formats=array(), $data_type=[], $avail_btn=true) {
+    function add_prepared_rec($conn, $table, array $columns, $formats=array(), $avail_btn=true) {
         $error = "";
         
         if(!empty($formats) && count($formats) > 0) {
@@ -111,11 +156,11 @@ class CRUD
         );
 
         foreach($columns as $key=>$val) {
-            if((!empty($data_type) && count($data_type) > 0) && isset($data_type[$key])) {
+            if("org_id" === $key) {
                 array_push($post_data['tcols'], "`{$key}`");
                 array_push($post_data['tvals'], "{$val}");
                 array_push($post_data['qmarks'], "?");
-                array_push($post_data['data_type'], "{$data_type[$key]}");
+                array_push($post_data['data_type'], "i");
             } else {
                 array_push($post_data['tcols'], "`{$key}`");
                 array_push($post_data['tvals'], "{$val}");
@@ -161,67 +206,103 @@ class CRUD
     *@param array $formats Additional form field format e.g: setting encrypting password field like array("password"=>md5('password'))
     *@param boolean $avail_btn Checks if submit button in the form has a name and is included in the post parameters
     */
-    public function update($table, array $columns, array $condition, $formats=array(), $data_type=[], $avail_btn=true) {
+    public function upd_rec_prepared($table, array $post_fields, array $clause, $formats=array(), $avail_btn=true) {
         global $conn;
         $error = "";
         
         if(!empty($formats) && count($formats) > 0) {
             foreach($formats as $fkey=>$fval) {
-                $columns[$fkey] = $fval;
+                $post_fields[$fkey] = $fval;
             }
-        }
-        
-        if($avail_btn === true) {
-            array_pop($columns);
         }
         
         $post_data = array(
-            "tsets" => array(),
-            "tvals" => array(),
-            "data_type" => array()
+            "psets" => array(),
+            "pvals" => array(),
+            "pb" => array(),
+            "vars"=>array()
         );
 
-        foreach($columns as $key=>$val) {
-            if((!empty($data_type) && count($data_type) > 0) && isset($data_type[$key])) {
-                array_push($post_data['tsets'], "`{$key}`=?");
-                array_push($post_data['tvals'], "{$val}");
-                array_push($post_data['data_type'], "{$data_type[$key]}");
-            } else {
-                array_push($post_data['tcols'], "`{$key}`=?");
-                array_push($post_data['tvals'], "{$val}");
-                array_push($post_data['data_type'], "s");
-            }
+        foreach($post_fields as $field=>$val) {
+            array_push($post_data['psets'], "`{$field}`=?");
+            array_push($post_data['pvals'], parse_str($val));
+            array_push($post_data['pb'], "s");
+            array_push($post_data['vars'], "{$field}=>{$val}");
+        }
+        
+        if($avail_btn === true) {
+            array_pop($post_data['psets']);
+            array_pop($post_data['pvals']);
+            array_pop($post_data['pb']);
         }
 
         //Conversions
-        $tsets = implode(", ", $post_data['tsets']);
-        //$qmarks = implode(", ", $post_data['qmarks']);
-        $data_type = implode("", $post_data['data_type']);
-        $params = array($data_type);
-        $k=0;
-        foreach($post_data['tsets'] as $f=>$v) {
-            $k++;
-            $params[$k] = & $post_data['tvals'][$f];
-            
-        }
+        $pcols = implode(", ", $post_data['psets']);
+        $pdata = implode(", ", $post_data['pvals']);
+        $pbind = implode("", $post_data['pb']);
 
-        $stmt = $conn->prepare("UPDATE {$table} SET {$tsets} WHERE {$condition['key']}='{$condition['val']}'");
-        //$stmt->bind_param($pbind, $pcols);
-        call_user_func_array(array($stmt, "bind_param"), $params);
+        $stmt = $conn->prepare("UPDATE {$table} SET {$pcols} WHERE {$clause['clause']}='{$clause['value']}'");
+        $stmt->bind_param($pbind, $pcols);
         
         if($stmt->execute()) {
-            return true;
+            return "Done";
         }
         
         $stmt->close();
         $conn->close();
+//        return "UPDATE {$table} SET {$pcols} WHERE {$clause['clause']}='{$clause['value']}'";
     }
     
     /**
-    * Performs User login
     *
-    * @param $query
-    * @return array if true and bool(false) if false
+    * Updates existing database table record(s) from form or any key=>value pair record
+    *
+    *@param string $table The database table to be updated
+    *@param array $post_fields http method(post, get) or key=>value pair
+    *@param string $clause Clause to be used for update
+    *@param array $formats Additional form field format e.g: setting encrypting password field like array("password"=>md5('password'))
+    *@param boolean $avail_btn Checks if submit button in the form has a name and is included in the post parameters
+    */
+    public function upd_rec($table, array $post_fields, array $clause, array $formats=null, $avail_btn=true) {
+        global $conn;
+        $error = "";
+        
+        if(!empty($formats) && count($formats) > 0) {
+            foreach($formats as $fkey=>$fval) {
+                $post_fields[$fkey] = $fval;
+            }
+        }
+        
+        $post_data = array(
+            "psets" => array()
+        );
+
+        foreach($post_fields as $field=>$val) {
+            array_push($post_data['psets'], "`{$field}`='" .$conn->real_escape_string($val) . "'");
+        }
+        
+        if($avail_btn === true) {
+            array_pop($post_data['psets']);
+        }
+
+        //Conversions
+        $pcols = implode(", ", $post_data['psets']);
+
+        $stmt = "UPDATE {$table} SET {$pcols} WHERE {$clause['clause']}='{$clause['value']}'";
+        //die($stmt);
+        $result = $conn->query($stmt);
+        
+        if($result) {
+            return $result;
+        } else {
+            die($conn->error);
+        }
+    }
+    
+    /**
+    * Performs User loin
+    *
+    * @param $query 
     */
     public function login($query) {
         global $conn;
@@ -239,39 +320,8 @@ class CRUD
             
             return $row;
         } else {
-            return false;
+            return "Error";
         }
-    }
-    
-    /**
-    * Uploads Multiple File
-    *
-    * @param array $fieldname The files to be uploaded to the file system
-    * @param string $uploadto The path the files will be uploaded to
-    * @return array uploaded file(s) properties in an associative array
-    */
-    function upload($fieldname, $uploadto) {
-
-        $files=array();
-        $fdata=$_FILES[$fieldname];
-        if(is_array($fdata['name'])) {
-            for($i=0; $i<count($fdata['name']); ++$i) {
-                $files[]=array(
-                    'name'=>date('Ymd' . time()) . $fdata['name'][$i],
-                    'type'=>$fdata['type'][$i],
-                    'tmp_name'=>$fdata['tmp_name'][$i]
-                );
-            }
-        } else {
-            $files[]=$fdata;
-        }
-
-        foreach($files as $file) {
-            move_uploaded_file($file['tmp_name'], $uploadto . $file['name']);
-
-        }
-
-        return $files;
     }
 
     /**
